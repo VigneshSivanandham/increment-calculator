6030c3fb76d676851e92991e846a0989395a7fa9
Vignesh Sivanandham <vignesh.sivanandham@symphonyretailai.com>
2019-05-25 09:35:32 +0530

    Adding initial files for the project

---
+++ app.js
@@ -0,0 +1,15 @@
+let names = ["A","B","C"];
+new Vue({
+	el : "#app",
+	template : "#app-template",
+	data : { 
+		dataNames : names ,
+		beforeIncrement : '',
+		afterIncrement : ''
+	},
+	methods : {
+		calculatePercentage: function(){
+			
+		}
+	}
+});
